5df50c0b55dcdb4a9f08abc987cf345972d9a831
Karl O <karl@otness.com>
2012-01-16 12:47:15 -0600

    Add GamepadResult class to enable easy filtering of Gamepad joysticks.

---
+++ src/com/edinarobotics/utils/gamepad/GamepadResult.java
@@ -0,0 +1,42 @@
+package com.edinarobotics.utils.gamepad;
+
+/**
+ * Represents the state of both of a Gamepad's joysticks.
+ * Can be used to filter these joysticks (deadzoning, scaling, etc.).
+ */
+public class GamepadResult {
+    private double lx;
+    private double ly;
+    private double rx;
+    private double ry;
+    
+    /**
+     * Creates a GamepadResult from the cartesian coordinates of two joysticks.
+     * @param lx the left x-value.
+     * @param ly the left y-value.
+     * @param rx the right x-value.
+     * @param ry  the right y-value.
+     */
+    public GamepadResult(double lx,double ly,double rx,double ry){
+        this.lx = lx;
+        this.ly = ly;
+        this.rx = rx;
+        this.ry = ry;
+    }
+    
+    public double getLeftX(){
+        return lx;
+    }
+    
+    public double getLeftY(){
+        return ly;
+    }
+    
+    public double getRightX(){
+        return rx;
+    }
+    
+    public double getRightY(){
+        return ry;
+    }
+}
