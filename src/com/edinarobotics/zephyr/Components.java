1f133f8898e8433d59b19175c61d8116cf9ac83a
Karl O <karl@otness.com>
2012-01-16 13:32:04 -0600

    Add Jaguar for the shooter's motor to Components.

--- src/com/edinarobotics/zephyr/Components.java
+++ src/com/edinarobotics/zephyr/Components.java
@@ -13,14 +13,17 @@ public class Components {
     //PORT NUMBERS HERE!
     private static int LEFT_JAGUAR_PORT = 1;
     private static int RIGHT_JAGUAR_PORT = 2;
+    private static int SHOOTER_JAGUAR_PORT = 3;
     
     private static Components instance;
     public Jaguar leftJaguar;
     public Jaguar rightJaguar;
+    public Jaguar shooterJaguar;
     
     private Components(){
         leftJaguar = new Jaguar(LEFT_JAGUAR_PORT);
         rightJaguar = new Jaguar(RIGHT_JAGUAR_PORT);
+        shooterJaguar = new Jaguar(SHOOTER_JAGUAR_PORT);
     }
     
     public static Components getInstance(){
