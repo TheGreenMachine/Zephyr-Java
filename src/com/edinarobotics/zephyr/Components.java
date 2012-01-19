e2de3945184e0d6459f84e442b091db01fd058e3
Karl O <karl@otness.com>
2012-01-18 19:39:01 -0600

    Add shooter pneumatics components to Components.

--- src/com/edinarobotics/zephyr/Components.java
+++ src/com/edinarobotics/zephyr/Components.java
@@ -1,6 +1,8 @@
 package com.edinarobotics.zephyr;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
@@ -11,18 +13,25 @@ public class Components {
     private static final int LEFT_JAGUAR_PORT = 1;
     private static final int RIGHT_JAGUAR_PORT = 2;
     private static final int SHOOTER_JAGUAR_PORT = 3;
+    private static final int COMPRESSOR_PRESSURE_SENSOR = 1;
+    private static final int COMPRESSOR_SPIKE = 1;
+    private static final int BALL_LOAD_PISTON_SPIKE = 2;
     
     private static Components instance;
     public Jaguar leftJaguar;
     public Jaguar rightJaguar;
     public Jaguar shooterJaguar;
     public RobotDrive driveControl;
+    public Compressor compressor;
+    public Relay ballLoadPiston;
     
     private Components(){
         leftJaguar = new Jaguar(LEFT_JAGUAR_PORT);
         rightJaguar = new Jaguar(RIGHT_JAGUAR_PORT);
         shooterJaguar = new Jaguar(SHOOTER_JAGUAR_PORT);
         driveControl = new RobotDrive(leftJaguar,rightJaguar);
+        compressor  = new Compressor(COMPRESSOR_PRESSURE_SENSOR,COMPRESSOR_SPIKE);
+        ballLoadPiston = new Relay(BALL_LOAD_PISTON_SPIKE);
     }
     
     public static Components getInstance(){
