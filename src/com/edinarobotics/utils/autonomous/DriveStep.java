f976464f17a6237beb1ad980017d7132c5bc4790
Danny Gratzer <danny.gratzer@gmail.com>
2012-01-28 11:07:10 -0600

    Adds DriveStep to the autonomous code

---
+++ src/com/edinarobotics/utils/autonomous/DriveStep.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edinarobotics.utils.autonomous;
+
+import com.edinarobotics.zephyr.Zephyr;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ * @author Danny
+ */
+public class DriveStep extends AutonomousStep{
+    Zephyr robot;
+    
+    public DriveStep(Zephyr robot)
+    {
+        this.robot = robot;
+    }
+    
+   public void start()
+   {
+       robot.leftDrive = .5;
+       robot.rightDrive = .5;
+       for(int x = 0; x<2000; x++)
+       {
+           robot.mechanismSet();
+           Timer.delay(.001);
+           
+       }
+   }
+   public boolean isFinished()
+   {
+       return true;
+   }
+   public void stop()
+   {
+       robot.stop();
+   }
+    
+}
