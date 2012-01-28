b1dcd63202fb3fdaea4268904d7fa51da144550b
Karl O <karl@otness.com>
2012-01-28 14:03:25 -0600

    Remove auto-generated Netbeans comments from IdleStep.

--- src/com/edinarobotics/zephyr/autonomous/IdleStep.java
+++ src/com/edinarobotics/zephyr/autonomous/IdleStep.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.edinarobotics.zephyr.autonomous;
 import com.edinarobotics.utils.autonomous.*;
 import com.edinarobotics.zephyr.Zephyr;
