7f522d182e826f2f225ec3d7c9142388e0353115
Alex O'Neill <alexander4913@aol.com>
2012-02-10 18:34:04 -0600

    Added Sensor components wrapper class

---
+++ src/com/edinarobotics/zephyr/parts/SensorComponents.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.edinarobotics.zephyr.parts;
+
+/**
+ *
+ * @author GreenMachine
+ */
+public class SensorComponents {
+
+}
