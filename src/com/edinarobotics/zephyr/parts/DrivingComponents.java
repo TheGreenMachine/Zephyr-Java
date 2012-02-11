be7f2453da5537c28c58b3a109a41aad0a65a58f
Danny Gratzer <danny.gratzer@gmail.com>
2012-02-11 14:18:49 -0600

    Added javadoc comments.

--- src/com/edinarobotics/zephyr/parts/DrivingComponents.java
+++ src/com/edinarobotics/zephyr/parts/DrivingComponents.java
@@ -11,26 +11,32 @@ public class DrivingComponents {
     private Jaguar leftMotors;
     private Jaguar rightMotors;
     private Relay shifters;
-    /*
-     * Intializes leftMotors, rightMotors and shifters with left, right, and shifters
+    /**
+     * Initializes leftMotors, rightMotors and shifters
+     * @param left is used to initialize leftMotors.
+     * @param right is used to initialize rightMotors.
+     * @param shifters is used to initialize shifters.
      */
     public DrivingComponents(int left, int right, int shifters){
         leftMotors = new Jaguar(left);
         rightMotors = new Jaguar(right);
         this.shifters = new Relay(shifters);
     }
-    /*
-     * Set the left jaguars to left and the right jaguars to right
+    /**
+     * Set the jaguars.
+     * @param left sets the leftMotors jaguar.
+     * @param right sets the rightMotors jaguar
      */
     public void setDrivingSpeed(double left, double right){
         leftMotors.set(left);
         rightMotors.set(right);
     }
-    /*
-     * shift the shifters based on boolean high
+    /**
+     * shift the shifters.
+     * @param high determines the state, true raises them, false lowers them.
      */
     public void shift(boolean high){
-        shifters.set((high?Relay.Value.kReverse:Relay.Value.kOff));
+        shifters.set((high?Relay.Value.kReverse:Relay.Value.kForward));
     }
     
 }
--- src/com/edinarobotics/zephyr/parts/ShooterComponents.java
+++ src/com/edinarobotics/zephyr/parts/ShooterComponents.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.edinarobotics.zephyr.parts;
 
 import edu.wpi.first.wpilibj.Jaguar;
