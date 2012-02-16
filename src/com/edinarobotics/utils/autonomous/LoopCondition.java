cef00cc4bee0df0cd3df83d7a3025684af51c14d
Karl O <karl@otness.com>
2012-02-16 16:03:39 -0600

    Add LoopCondition to represent a condition of a loop.
    This class adds a reset() method to BooleanConditon so that the loop
    can be run multiple times.

---
+++ src/com/edinarobotics/utils/autonomous/LoopCondition.java
@@ -0,0 +1,15 @@
+package com.edinarobotics.utils.autonomous;
+
+/**
+ * Represents a {@link BooleanCondition} that can be used as the condition of
+ * a loop. This condition can be reset when re-entering the loop.
+ */
+public interface LoopCondition extends BooleanCondition{
+    /**
+     * Resets the loop condition. This method may be called before or after
+     * the loop has run.
+     * This method should reset the condition to its initial state if this
+     * is at all possible.
+     */
+    public void reset();
+}
