a63fe91221da4cd91a6cb46a9234d51f1394800e
Karl O <karl@otness.com>
2012-02-18 16:23:10 -0600

    Change SetConveyorStep to use the new constants to run the collector.

--- src/com/edinarobotics/zephyr/autonomous/SetConveyorStep.java
+++ src/com/edinarobotics/zephyr/autonomous/SetConveyorStep.java
@@ -2,6 +2,7 @@ package com.edinarobotics.zephyr.autonomous;
 
 import com.edinarobotics.utils.autonomous.AutonomousStep;
 import com.edinarobotics.zephyr.Zephyr;
+import com.edinarobotics.zephyr.parts.CollectorComponents;
 
 /**
  * An autonomous step that can be used to control the internal conveyor belt
@@ -28,7 +29,7 @@ public class SetConveyorStep extends AutonomousStep{
      * Sets the conveyor to the requested state, {@code conveyorOn}.
      */
     public void start(){
-        robot.convMove = conveyorOn;
+        robot.convMove = (conveyorOn?CollectorComponents.CONVEYOR_UP:CollectorComponents.CONVEYOR_STOP);
         robot.mechanismSet();
     }
     
