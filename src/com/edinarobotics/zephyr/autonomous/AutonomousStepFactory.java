6e6ae7c5baf8c37a3eededa9b6f75ee0f684d254
Karl O <karl@otness.com>
2012-04-26 08:40:13 -0500

    Add gyro turn steps to autonomous.

--- src/com/edinarobotics/zephyr/autonomous/AutonomousStepFactory.java
+++ src/com/edinarobotics/zephyr/autonomous/AutonomousStepFactory.java
@@ -1,6 +1,7 @@
 package com.edinarobotics.zephyr.autonomous;
 
 import com.edinarobotics.utils.autonomous.*;
+import com.edinarobotics.zephyr.Components;
 import com.edinarobotics.zephyr.Zephyr;
 
 /**
@@ -8,6 +9,7 @@ import com.edinarobotics.zephyr.Zephyr;
  */
 public class AutonomousStepFactory {
     private Zephyr robot;
+    private final double DEFAULT_GYRO_TURN_SPEED = 80;
     
     public AutonomousStepFactory(Zephyr robot){
         this.robot = robot;
@@ -48,5 +50,72 @@ public class AutonomousStepFactory {
         warmupSteps[2] = new ShooterVoltagePWMStep(targetVelocity, robot);
         warmupSteps[3] = new IdleWaitStep(estimate2Delay,robot);
         return new SequentialAutonomousStepGroup(warmupSteps);
+    }
+    
+    /**
+     * Creates a step that performs a turn of a certain number of degrees
+     * using the gyro.
+     * @param degreesLeft The number of degrees that the robot should
+     * turn left.
+     * @return An {@link AutonomousStep} that performs a left turn
+     * of a certain number of degrees using the gyro.
+     */
+    public AutonomousStep turnLeftStep(double degreesLeft){
+        return getGyroTurnSteps(-degreesLeft, DEFAULT_GYRO_TURN_SPEED);
+    }
+    
+    /**
+     * Creates a step that performs a turn of a certain number of degrees
+     * using the gyro.
+     * @param degreesLeft The number of degrees that the robot should
+     * turn left.
+     * @param power The power to be given to both motors when performing the
+     * turn.
+     * @return An {@link AutonomousStep} that performs a left turn
+     * of a certain number of degrees using the gyro and the given power.
+     */
+    public AutonomousStep turnLeftStep(double degreesLeft, double power){
+        return getGyroTurnSteps(degreesLeft, power);
+    }
+    
+    /**
+     * Creates a step that performs a turn of a certain number of degrees
+     * using the gyro.
+     * @param degreesLeft The number of degrees that the robot should
+     * turn left.
+     * @return An {@link AutonomousStep} that performs a left turn
+     * of a certain number of degrees using the gyro.
+     */
+    public AutonomousStep turnRightStep(double degreesRight){
+        return getGyroTurnSteps(degreesRight, DEFAULT_GYRO_TURN_SPEED);
+    }
+    
+    public AutonomousStep turnRightStep(double degreesRight, double power){
+        return getGyroTurnSteps(degreesRight, power);
+    }
+    
+    /**
+     * Returns an autonomous step that performs a turn using a gyro.
+     * @param angle The angle to turn. Negative values represent counterclockwise
+     * turns.
+     * @param power The power for the chassis to use to turn.
+     */
+    private AutonomousStep getGyroTurnSteps(double angle, double power){
+        double workingPower = Math.abs(power);
+        double leftPower = 0;
+        double rightPower = 0;
+        if(angle<=0){
+            leftPower = -workingPower;
+            rightPower = workingPower;
+        }
+        else{
+            leftPower = workingPower;
+            rightPower = -workingPower;
+        }
+        AutonomousStep[] turnSteps = new AutonomousStep[3];
+        turnSteps[0] = new SetDriveSpeedStep(leftPower, rightPower, robot);
+        turnSteps[1] = new GyroWaitStep(angle, Components.getInstance().gyro, robot);
+        turnSteps[2] = new SetDriveSpeedStep(0, 0, robot);
+        return new SequentialAutonomousStepGroup(turnSteps);
     }
 }
